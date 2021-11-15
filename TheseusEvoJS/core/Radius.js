e00171dc903f374b24668cc4acdf12fe2d90fa82
TTBK23 <32800017+TTBK23@users.noreply.github.com>
2021-11-15 14:25:54 +0100

    delete

--- TheseusEvoJS/core/Radius.js
+++
@@ -1,9 +0,0 @@
-class Radius {
-  constructor(x1, y1, x2, y2, angle, radType)  {
-    this.capNodes = [];
-    this.x1 = x1; this.y1 = y1; this.x2 = x2; this.y2 = y2;
-    this.angle = angle;
-    this.radType = radType;
-    this.length=Math.sqrt(x2*x2+y2*y2)+0.5;
-  }
-}
